a2abd0c203be00f5f2b3d597150ac01a4485098c
morykwon <kwonmory@gmail.com>
2020-05-12 22:24:10 +0900

    PS 코드 추가
    * [프로그래머스] 최댓값과 최솟값 테스트 케이스 추가

--- src/programmers/lv2/P최댓값과_최솟값/solution/my01.js
+++ src/programmers/lv2/P최댓값과_최솟값/solution/my01.js
@@ -3,3 +3,8 @@ function solution(s) {
 
     return `${Math.min.apply(null, sArr)} ${Math.max.apply(null, sArr)}`;
 }
+
+module.exports = solution;
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/12939
+ */
