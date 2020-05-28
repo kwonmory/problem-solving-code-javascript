f5452d95b410590e92a50675a21244e5513534c5
morykwon <kwonmory@gmail.com>
2020-05-29 00:22:07 +0900

    PS 코드 추가
    * [프로그래머스] 서울에서 김서방 찾기

---
+++ src/programmers/lv1/P서울에서_김서방_찾기/solution/p1.js
@@ -0,0 +1,9 @@
+function solution(seoul) {
+    return `김서방은 ${seoul.indexOf('Kim')}에 있다`;
+}
+
+export default solution;
+
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/12919?language=javascript
+ */
---
+++ src/programmers/lv1/P서울에서_김서방_찾기/solution/p1.test.js
@@ -0,0 +1,5 @@
+import solution from "./p1";
+
+test('김서방 찾기 테스트', () => {
+    expect(solution(['Jane', 'Kim'])).toBe('김서방은 1에 있다');
+})
